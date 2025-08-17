d79a01b846a63dcb47ec0dd0cf19f676495b8e16
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2025-08-17 22:38:39 +0200

    feat: implement changes to have component compute the stress field when moment applied

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import Rhino
-import time
 
 from joint_calc import face, joint, stress_field, geometry
 
@@ -14,9 +13,8 @@ for i, brep_face in enumerate(brep_faces):
         id=i,
         parent_joint_id=0,
         area=face_area,
-        normal=geometry.Vector(brep_face.Faces[0].NormalAt(0.5, 0.5).X, brep_face.Faces[0].NormalAt(0.5, 0.5).Y, brep_face.Faces[0].NormalAt(0.5, 0.5).Z),
-        brep_surface=brep_face,
-        resultant_location=None
+        normal=geometry.Vector.from_vector_3d(brep_face.Faces[0].NormalAt(0.5, 0.5)),
+        brep_surface=brep_face
     )
     joint_faces.append(joint_face)
 
@@ -26,12 +24,12 @@ my_joint = joint.Joint(name="Joint1", faces=joint_faces, moment_weights=None)
 
 stress_computer = stress_field.StressFieldComputer(
     moment=geometry.Vector(moment_vector.X, moment_vector.Y, moment_vector.Z),
-    rotation_point=geometry.Vector(rotation_center.X, rotation_center.Y, rotation_center.Z),
+    rotation_point=geometry.Point(rotation_center.X, rotation_center.Y, rotation_center.Z),
     joint=my_joint
 )
 stress_computer.compute_stress_field()
 
 for face in my_joint.faces:
     anchors.append(Rhino.Geometry.Point3d(face.resultant_location.x, face.resultant_location.y, face.resultant_location.z))
-
-print(my_joint.moment_weights)
+stress_distributions = [face.stress_distribution for face in my_joint.faces]
+sigma_maxs = [f"sigma_max = {round(face.max_stress/1e6, 2)} MPa" for face in my_joint.faces]
