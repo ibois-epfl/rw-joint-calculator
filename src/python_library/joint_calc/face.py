8d3ab4219f7ad3af34177c7bc44825c4fae75fed
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2025-08-24 17:50:22 +0200

    feat: add joint face axis detection

--- src/python_library/joint_calc/face.py
+++ src/python_library/joint_calc/face.py
@@ -19,3 +19,28 @@ class JointFace:
     resultant_location: geometry.Point = None  # The location of the resultant force vector that will be applied on this face
     stress_distribution: Rhino.Geometry.Brep = None
     max_stress: float = 0.0
+    main_axis: geometry.Vector = None
+
+    def __post_init__(self):
+        self.__detect_main_axis()
+
+    def __detect_main_axis(self):
+        """
+        Detect the main axis of the face based on its normal vector.
+        """
+        edges = [
+            geometry.Vector.from_vector_3d(edge.PointAtEnd - edge.PointAtStart)
+            for edge in self.brep_surface.Edges
+        ]
+        for first_candidate_edge_vector in edges:
+            if first_candidate_edge_vector.z < 0:
+                first_candidate_edge_vector = first_candidate_edge_vector * -1
+            for second_candidate_edge_vector in edges:
+                if second_candidate_edge_vector.z < 0:
+                    second_candidate_edge_vector = second_candidate_edge_vector * -1
+                # Check if the two candidate vectors are orthogonal
+                if first_candidate_edge_vector.is_parallel_to(
+                    second_candidate_edge_vector, 0.1
+                ):
+                    self.main_axis = first_candidate_edge_vector
+                    return
