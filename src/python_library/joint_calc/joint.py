52e84ac29b207154dcc7306c23a58276528e7cd1
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2025-11-20 00:10:50 +0100

    feat-wip: update Joint dataclass now with normal harmonisation

--- src/python_library/joint_calc/joint.py
+++ src/python_library/joint_calc/joint.py
@@ -4,11 +4,20 @@ This is a module to store joints with their faces
 
 from dataclasses import dataclass
 
-from joint_calc import face
+import joint_calc
 
 
 @dataclass
 class Joint:
-    name: str
-    faces: list[face.JointFace]
-    moment_weights: list[float]  # How much this face contributes to the overall moment
+    id: int
+    original_faces: list[joint_calc.face.JointFace]
+    moment_vector: joint_calc.geometry.Vector
+    working_faces: list[joint_calc.face.JointFace] = None
+
+    def __post_init__(self):
+        for joint_face in self.original_faces:
+            rh_joint_brep_face = joint_face.rh_joint_brep_face
+            rh_normal = rh_joint_brep_face.NormalAt(0, 0)
+            if rh_normal * self.moment_vector.to_vector_3d() < 0:
+                print("Inverting normal vector.")
+                rh_normal *= -1
