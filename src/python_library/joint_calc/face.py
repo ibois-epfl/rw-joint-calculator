f86b246b8ad2c70f46e8b488b2db8a452901b43f
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2025-11-20 00:06:46 +0100

    feat-wip: update JointFace dataclass with area and centroid computed in post_init

--- src/python_library/joint_calc/face.py
+++ src/python_library/joint_calc/face.py
@@ -4,18 +4,25 @@ This module contains the dataclass for joint faces
 
 from dataclasses import dataclass
 
-from joint_calc import geometry
-
 import Rhino
+
+import joint_calc
 
 
 @dataclass
 class JointFace:
+    """
+    A dataclass to store joint face information.
+    JointFaces are assumed to be flat
+    """
+
     id: int
     parent_joint_id: int
-    area: float
-    normal: geometry.Vector
-    brep_surface: Rhino.Geometry.Brep
-    resultant_location: geometry.Point = None  # The location of the resultant force vector that will be applied on this face
+    rh_joint_brep_face: Rhino.Geometry.BrepFace
+    resultant_location: joint_calc.geometry.Point = None  # The location of the resultant force vector that will be applied on this face
     stress_distribution: Rhino.Geometry.Brep = None
     max_stress: float = 0.0
+
+    def __post_init__(self):
+        self.area = joint_calc.utils.compute_area(self.rh_joint_brep_face)
+        self.normal = self.rh_joint_brep_face.NormalAt(0, 0)
