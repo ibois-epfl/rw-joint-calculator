d0cf147c56fbb15059a0688e93e731a9380d9c00
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2025-11-19 23:44:10 +0100

    feat: add utils module to joint_calc

---
+++ src/python_library/joint_calc/utils.py
@@ -0,0 +1,22 @@
+""" "This module collects utility functions, especially for Rhino-specific operations."""
+
+import Rhino
+
+from joint_calc import geometry
+
+
+def compute_area(brep_face: Rhino.Geometry.Brep) -> float:
+    """Compute the area of a Brep face."""
+    area_properties = Rhino.Geometry.AreaMassProperties.Compute(brep_face)
+    if area_properties is None:
+        raise ValueError("Could not compute area for the given Brep face.")
+    return area_properties.Area
+
+
+def compute_centroid(brep_face: Rhino.Geometry.Brep) -> geometry.Point:
+    """Compute the centroid of a Brep face."""
+    area_properties = Rhino.Geometry.AreaMassProperties.Compute(brep_face)
+    if area_properties is None:
+        raise ValueError("Could not compute centroid for the given Brep face.")
+    centroid_3d = area_properties.Centroid
+    return geometry.Point.from_Point3d(centroid_3d)
