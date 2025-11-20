28c20469ec7bfcc618c790c7fd022ce3dde1b73a
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2025-11-20 11:53:18 +0100

    feat: add working face detection in Joint dataclass

--- src/python_library/joint_calc/joint.py
+++ src/python_library/joint_calc/joint.py
@@ -2,6 +2,8 @@
 This is a module to store joints with their faces
 """
 
+import Rhino
+
 from dataclasses import dataclass
 
 import joint_calc
@@ -12,12 +14,65 @@ class Joint:
     id: int
     original_faces: list[joint_calc.face.JointFace]
     moment_vector: joint_calc.geometry.Vector
+    rotation_point: joint_calc.geometry.Point
     working_faces: list[joint_calc.face.JointFace] = None
+
+    def detect_working_faces(self):
+        self.working_faces = []
+        for joint_face in self.original_faces:
+            plane = Rhino.Geometry.Plane(
+                self.rotation_point.to_point_3d(),
+                self.moment_vector.to_vector_3d(),
+                joint_face.rh_normal,
+            )
+
+            success, rh_crvs, rh_pts = Rhino.Geometry.Intersect.Intersection.BrepPlane(
+                joint_face.rh_joint_brep_face.Brep,
+                plane,
+                Rhino.RhinoDoc.ActiveDoc.ModelAbsoluteTolerance,
+            )
+
+            if success and rh_crvs:
+                rh_curve = rh_crvs[0]
+                rh_candidate_surfaces = joint_face.rh_joint_brep_face.Brep.Split(
+                    [rh_curve], Rhino.RhinoDoc.ActiveDoc.ModelAbsoluteTolerance
+                )
+                candidate_surfaces_centroids = [
+                    joint_calc.utils.compute_centroid(srf)
+                    for srf in rh_candidate_surfaces
+                ]
+                for idx, centroid in enumerate(candidate_surfaces_centroids):
+                    if (
+                        Rhino.Geometry.Vector3d.CrossProduct(
+                            joint_face.rh_normal,
+                            Rhino.Geometry.Vector3d(
+                                centroid.to_point_3d()
+                                - self.rotation_point.to_point_3d()
+                            ),
+                        )
+                        * self.moment_vector.to_vector_3d()
+                        < 0
+                    ):
+                        self.working_faces.append(rh_candidate_surfaces[idx])
+
+            else:
+                if (
+                    Rhino.Geometry.Vector3d.CrossProduct(
+                        joint_face.rh_normal,
+                        Rhino.Geometry.Vector3d(
+                            joint_face.centroid.to_point_3d()
+                            - self.rotation_point.to_point_3d()
+                        ),
+                    )
+                    * self.moment_vector.to_vector_3d()
+                    < 0
+                ):
+                    self.working_faces.append(joint_face.rh_joint_brep_face.Brep)
 
     def __post_init__(self):
         for joint_face in self.original_faces:
-            rh_joint_brep_face = joint_face.rh_joint_brep_face
-            rh_normal = rh_joint_brep_face.NormalAt(0, 0)
-            if rh_normal * self.moment_vector.to_vector_3d() < 0:
+            if joint_face.rh_normal * self.moment_vector.to_vector_3d() < 0:
                 print("Inverting normal vector.")
-                rh_normal *= -1
+                joint_face.rh_normal *= -1
+
+        self.detect_working_faces()
