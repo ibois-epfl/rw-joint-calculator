2f39f067d3aa6b3eb8b0f2c37fc9467d0eb7501c
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2025-11-24 11:50:46 +0100

    fix: normal renamed to rh_normal and centroid computed at JointFace instanciation

--- src/python_library/joint_calc/face.py
+++ src/python_library/joint_calc/face.py
@@ -20,9 +20,9 @@ class JointFace:
     parent_joint_id: int
     rh_joint_brep_face: Rhino.Geometry.BrepFace
     resultant_location: joint_calc.geometry.Point = None  # The location of the resultant force vector that will be applied on this face
-    stress_distribution: Rhino.Geometry.Brep = None
     max_stress: float = 0.0
 
     def __post_init__(self):
         self.area = joint_calc.utils.compute_area(self.rh_joint_brep_face)
-        self.normal = self.rh_joint_brep_face.NormalAt(0, 0)
+        self.rh_normal = self.rh_joint_brep_face.NormalAt(0, 0)
+        self.centroid = joint_calc.utils.compute_centroid(self.rh_joint_brep_face)
