bc8efe382cb846265b56ea7983e2264b1f625ba3
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2026-06-03 12:01:27 +0200

    feat: add utils method for E modulus calculation at given grain angle

--- src/python_library/joint_calc/utils.py
+++ src/python_library/joint_calc/utils.py
@@ -2,6 +2,8 @@
 
 import Rhino
 
+import math
+
 from . import geometry
 
 
@@ -20,3 +22,20 @@ def compute_centroid(brep_face: Rhino.Geometry.Brep) -> geometry.Point:
         raise ValueError("Could not compute centroid for the given Brep face.")
     centroid_3d = area_properties.Centroid
     return geometry.Point.from_Point3d(centroid_3d)
+
+
+def compute_young_modulus(
+    grain_orientation: geometry.Vector,
+    face_normal: geometry.Vector,
+    E0: float,
+    E90: float,
+) -> float:
+    """Compute the Young's modulus based on the grain orientation using a simple interpolation."""
+    # Following Hankinson's formula:
+    theta = grain_orientation.compute_angle_with(face_normal)
+    if theta == 0:
+        return E0
+    elif theta == math.pi / 2:
+        return E90
+    else:
+        return (E0 * E90) / (E0 * math.sin(theta) ** 2 + E90 * math.cos(theta) ** 2)
