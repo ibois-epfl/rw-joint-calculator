1c5821927929bd32d70c3a9343ca9c51d7d0f677
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2026-06-10 01:48:56 +0200

    feat: create JointSubFace class

--- src/python_library/joint_calc/face.py
+++ src/python_library/joint_calc/face.py
@@ -3,10 +3,37 @@ This module contains the dataclass for joint faces
 """
 
 from dataclasses import dataclass
+import math
 
 import Rhino
 
 from . import geometry, utils
+
+
+@dataclass
+class JointSubFace:
+    """
+    A dataclass to store joint sub-face information.
+    JointSubFaces are the result of subdividing JointFaces into smaller faces for more accurate stress calculations.
+    """
+
+    id: int
+    parent_joint_face_id: int
+    rh_joint_brep_face: Rhino.Geometry.BrepFace
+    rh_normal: Rhino.Geometry.Vector3d
+    mesh: Rhino.Geometry.Mesh = None
+    Young_modulus: float = None
+    max_bending_stress: float = 0.0
+    axial_stress: float = 0.0
+    location_of_max_stress: geometry.Point = None
+    area: float = None
+    centroid: geometry.Point = None
+    effective_depth: float = None
+
+    def __post_init__(self):
+        self.area = utils.compute_area(self.rh_joint_brep_face)
+        self.centroid = utils.compute_centroid(self.rh_joint_brep_face)
+        self.effective_depth = math.sqrt(self.area)
 
 
 @dataclass
@@ -19,11 +46,12 @@ class JointFace:
     id: int
     parent_joint_id: int
     rh_joint_brep_face: Rhino.Geometry.BrepFace
-    max_stress: float = 0.0
+    max_stress: float = -1.0
     effective_depth: float = None
     Young_modulus: float = None
     location_of_max_stress: geometry.Point = None
-    mesh: Rhino.Geometry.Mesh = None
+    bending_subface: JointSubFace = None
+    axial_subface: JointSubFace = None
 
     def __post_init__(self):
         self.area = utils.compute_area(self.rh_joint_brep_face)
@@ -49,3 +77,15 @@ class JointFace:
         return geometry.Vector.from_vector_3d(
             area_properties.WorldCoordinatesSecondMoments
         )
+
+    @property
+    def cumulated_stresses(self):
+        sigma_bending = (
+            self.bending_subface.max_bending_stress
+            if self.bending_subface is not None
+            else 0.0
+        )
+        sigma_axial = (
+            self.axial_subface.axial_stress if self.axial_subface is not None else 0.0
+        )
+        return sigma_bending + sigma_axial
